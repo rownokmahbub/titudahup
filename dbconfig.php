07ea97c9a3a2538417c7bd6b3c10176be4e6e85d
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-09-24 15:22:54 +0600

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+
+// Connect to the MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "titudahup";
+
+// Create connection
+$con= mysqli_connect($servername,$username,$password,$dbname) or die("Query died: connect");
+mysqli_query($con,'SET CHARACTER SET utf8'); 
+mysqli_query($con,"SET SESSION collation_connection ='utf8_general_ci'");
